17e543423330b83eb9e454c41ea9e5274924a9ba
Luiz Fernando Franzon <luizfranzon05@hotmail.com>
2024-10-13 11:54:03 -0300

    add: create qr-code from text component

--- src/app/screens/create/create.page.ts
+++ src/app/screens/create/create.page.ts
@@ -1,30 +1,54 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { QRCodeModule } from 'angularx-qrcode';
 import {
-  IonContent,
   IonHeader,
-  IonTitle,
   IonToolbar,
+  IonTitle,
+  IonContent,
+  IonText,
+  IonTextarea,
+  IonCard,
+  IonCardContent,
+  IonThumbnail,
+  IonButton,
+  IonLabel,
   IonIcon,
 } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { saveOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.page.html',
-  standalone: true,
   imports: [
     IonIcon,
+    IonLabel,
+    IonButton,
+    IonCardContent,
+    IonCard,
+    IonTextarea,
+    IonText,
     IonContent,
-    IonHeader,
     IonTitle,
     IonToolbar,
+    IonHeader,
+    IonThumbnail,
     CommonModule,
     FormsModule,
+    QRCodeModule,
   ],
+  standalone: true,
 })
-export class CreatePage implements OnInit {
-  constructor() {}
+export class CreatePage {
+  textToBeEncoded = signal<string>('');
 
-  ngOnInit() {}
+  onTextChange(event: Event) {
+    this.textToBeEncoded.set((event.target as HTMLTextAreaElement).value);
+  }
+
+  constructor() {
+    addIcons({ saveOutline });
+  }
 }
